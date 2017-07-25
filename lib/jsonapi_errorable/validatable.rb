51d2c2f5578521ccd8fe4311d2fba471ab9e0a87
Cyril Duchon-Doris <Startouf@users.noreply.github.com>
2017-07-25 19:39:50 +0200

    Add a keyword parameter to override relationships to be rendered for jsonapi errors
    This especially allows to render jsonapi errors without a jsonapi payload (such as GET requests) by passing `relationships: {}`

--- lib/jsonapi_errorable/validatable.rb
+++ lib/jsonapi_errorable/validatable.rb
@@ -1,8 +1,13 @@
 module JsonapiErrorable
   module Validatable
-    def render_errors_for(record)
-      validation = Serializers::Validation.new \
-        record, deserialized_params.relationships
+    # @param relationships: nil [Hash] list of relationships to be serialized as errors
+    # @param record [ ActiveModel ] Object that implements ActiveModel
+    def render_errors_for(record, relationships: nil)
+      validation = Serializers::Validation.new(
+        record,
+        relationships || deserialized_params.relationships
+      )
+      
 
       render \
         json: { errors: validation.errors },
