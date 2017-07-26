129f7926e32ceec8f0c3bc242757e9bc4ddab3e0
Cyril Duchon-Doris <cyril.duchon-doris@telecom-paristech.org>
2017-07-26 11:55:09 +0200

    Switch back to previous implementation

--- lib/jsonapi_errorable/validatable.rb
+++ lib/jsonapi_errorable/validatable.rb
@@ -3,13 +3,8 @@ module JsonapiErrorable
     # @param relationships: nil [ Hash or FalseClass ] list of relationships whose errors should be serialized
     #   Defaults to the deserialized data.relationships of Json:api Payload
     # @param record [ ActiveModel ] Object that implements ActiveModel
-    def render_errors_for(record, relationships: nil)
-      relationships =
-        if relationships == false
-          {}
-        else
-          relationships || deserialized_params.relationships
-        end
+    def render_errors_for(record, relationships: {})
+      relationships || deserialized_params.relationships
 
       validation = Serializers::Validation.new(record, relationships)
 
